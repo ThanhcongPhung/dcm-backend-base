960b1357d1add3928a6765f19debc4a5779192fc
TakarinTD <56709998+TakarinTD@users.noreply.github.com>
2021-07-07 14:53:10 +0700

    api getUser

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,7 +6,13 @@ const updateUser = async (req, res) => {
   const user = await userService.updateUser(userId, updateFields);
   return res.send({ status: 1, result: user });
 };
+const getUser = async (req, res) => {
+  const { userId } = req.params;
+  const user = await userService.getUser(userId);
+  return res.send({ status: 1, result: user });
+};
 
 module.exports = {
   updateUser,
+  getUser,
 };
